862641671e08fd8502fef50b5c34bfc650cb6071
TerryD4vis <baphome@gmail.comt>
2022-01-17 19:02:41 +0100

    removing WTForm, replace it with own code

--- pymin/pymin/views.py
+++ pymin/pymin/views.py
@@ -11,22 +11,41 @@ def index():
     return render_template('index.html')
 
 
+# @bp.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('views.index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('views.login'))
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#         if not next_page or url_parse(next_page).netloc != '':
+#             next_page = url_for('views.index')
+#         return redirect(next_page)
+#     return render_template('login.html', form=form)
+
+
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('views.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('views.login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('views.index')
-        return redirect(next_page)
-    return render_template('login.html', form=form)
+    if request.method == 'POST':
+         username = request.form['username']
+         pw = request.form['pw']
+         user = User.query.filter_by(username=username).first()
+         if user is None or not user.check_password(pw):
+             flash('Invalid username or password')
+             return redirect(url_for('views.login'))
+         login_user(user)
+         next_page = request.args.get('next')
+         if not next_page or url_parse(next_page).netloc != '':
+             next_page = url_for('views.index')
+         return redirect(next_page)
+    return render_template('login.html')
 
 
 @bp.route('/logout')
